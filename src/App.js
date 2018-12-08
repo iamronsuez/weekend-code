318677661c8b02625284e4de3f88e7a4dc27bf8e
Ronald Suez <rsuez@mediastre.am>
2018-12-08 15:02:55 -0300

    probando

--- src/App.js
+++ src/App.js
@@ -61,7 +61,22 @@ class App extends Component {
             Learn React -> llovizna labs
           </a>
 
-          <Form />
+          <form name="contact" method="POST" netlify>
+            <div className="form-group">
+              <label htmlFor="exampleInputEmail1">Email address</label>
+              <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+              <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+            </div>
+            <div className="form-group">
+              <label htmlFor="exampleInputPassword1">Password</label>
+              <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+            </div>
+            <div className="form-check">
+              <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+              <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+          </form>
         </header>
 
         {/* <div>
