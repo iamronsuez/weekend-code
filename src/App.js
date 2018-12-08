51933712283949c42119e044b0e319e327170e75
Ronald Suez <rsuez@mediastre.am>
2018-12-08 15:20:28 -0300

    added proper forms

--- src/App.js
+++ src/App.js
@@ -25,19 +25,21 @@ const TodoItem = ({text, checked}) => {
 const Form = () => {
   return (
     <form name="contact" method="POST">
+
+     <div className="form-group">
+        <label htmlFor="exampleInputPassword1">Nombre</label>
+        <input name="input"  type="password" className="form-control" id="exampleInputPassword1" placeholder="name" />
+      </div>
       <div className="form-group">
         <label htmlFor="exampleInputEmail1">Email address</label>
-        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+        <input name="email" type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
         <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
       </div>
       <div className="form-group">
-        <label htmlFor="exampleInputPassword1">Password</label>
-        <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+        <label htmlFor="exampleInputPassword1">Message</label>
+        <textarea className="form-control" name="message" id="exampleInputPassword1" placeholder="message" />
       </div>
-      <div className="form-check">
-        <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-        <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-      </div>
+   
       <input type="hidden" name="form-name" value="contact" />
       <button type="submit" className="btn btn-primary">Submit</button>
     </form>
