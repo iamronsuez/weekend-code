c06633b64206bf6671e7c7a3bd65756f4071cc64
Ronald Suez <rsuez@mediastre.am>
2018-12-08 15:13:35 -0300

    fix form react

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const TodoItem = ({text, checked}) => {
 
 const Form = () => {
   return (
-    <form name="contact" method="POST" netlify>
+    <form name="contact" method="POST">
       <div className="form-group">
         <label htmlFor="exampleInputEmail1">Email address</label>
         <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
@@ -38,6 +38,7 @@ const Form = () => {
         <input type="checkbox" className="form-check-input" id="exampleCheck1" />
         <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
       </div>
+      <input type="hidden" name="form-name" value="contact" />
       <button type="submit" className="btn btn-primary">Submit</button>
     </form>
   )
@@ -47,40 +48,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code>  hola
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React -> llovizna labs
-          </a>
-        </header>
-
-        <div>
-
-        <form name="contact" method="POST" netlify>
-            <div className="form-group">
-              <label htmlFor="exampleInputEmail1">Email address</label>
-              <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-              <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
-            </div>
-            <div className="form-group">
-              <label htmlFor="exampleInputPassword1">Password</label>
-              <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
-            </div>
-            <div className="form-check">
-              <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-              <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-            </div>
-            <button type="submit" className="btn btn-primary">Submit</button>
-          </form>
-
+        <div className="App-header">
+            <Form />
         </div>
       </div>
     );
