7e37ae8fbd21fbf771e5bfea638d4a9746f487ae
Ronald Suez <ronald.suezascanio@cencosud.cl>
2019-09-18 12:35:31 -0300

    added references info

--- src/functions/price.js
+++ src/functions/price.js
@@ -1,5 +1,5 @@
 import fetch from 'node-fetch'
-import {get, upperCase} from 'lodash'
+import {get, upperCase, map} from 'lodash'
 
 const API_ENDPOINT = "https://localbitcoins.com/bitcoinaverage/ticker-all-currencies/"
 
@@ -12,16 +12,29 @@ exports.handler = async (event, context) => {
   return fetch(API_ENDPOINT)
     .then(response => response.json())
     .then(data => {
+      const currencies = [
+        {base: 'VES', divider: 'CLP'},
+        {base: 'CLP', divider: 'VES'},
+        {base: 'USD', divider: 'VES'},
+        {base: 'USD', divider: 'CLP'},
+        {base: 'PAB', divider: 'USD'},
+        {base: 'PAB', divider: 'VES'},
+        {base: 'USD', divider: 'PAB'},
+      ]
+
       const baseData =  getCurrencyData(get(data,upperCase(base), 'USD'), 'rates.last')
       const dividerData = getCurrencyData(get(data, upperCase(divider), 'USD'),'rates.last')
+      const references = map(currencies, ({base, divider}) => ({rel: getRelation(base, divider), base, divider}))
       try {
         return {
           statusCode: 200,
-          body: JSON.stringify({currencies: {
+          body: JSON.stringify({
+            currencies: {
               base: baseData,
               divider: dividerData,
               relation: getRelation(baseData, dividerData)
-            }
+            },
+            references
           }, null, 2)
         }
       } catch (error) {
